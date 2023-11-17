7f15284ab009cdca68e27cd353c9df7d86a10056
JK-codinghub <jyotickalashetty@gmail.com>
2023-11-17 14:01:40 +0530

    Project Initialized

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="col-2 bg-warning">23</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
