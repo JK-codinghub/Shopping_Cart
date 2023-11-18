0bafd6b81404da5f647a0796694859dba82871e8
JK-codinghub <jyotickalashetty@gmail.com>
2023-11-18 19:28:15 +0530

    Category Loop defined and UI implemented

--- src/components/ProductCategory.vue
+++ src/components/ProductCategory.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="row gx-0 bg-warning">
-    <div class="col-2">
+  <div class="row gx-0">
+    <div class="col-2" v-for="(category,index) in categoryLoop" :key="index">
         <div class="row">
-            <div class="col-12" :class="categoryLoop[0].class">
+            <div class="col-12" :class="category.class">
              <!-- <img :src="categoryLoop[0].path" width="170" height="170" alt="">     -->
             </div>       
-            <div class="col-12 d-flex justify-content-start categoryStyle">{{categoryLoop[0].name}}</div>
+            <div class="col-12 d-flex justify-content-start categoryStyle">{{category.name}}</div>
         </div>
     </div>
   </div>
@@ -17,7 +17,12 @@ export default {
  name:"ProductCategory",
  setup() {
     const categoryLoop = ref([
-        {name:"Men’s T-Shirtsgory1", class:"ellipse1", path:"../assets/Ellipse 7.png"}
+        {name:"Men’s T-Shirtsgory1", class:"ellipse category1", path:"../assets/Ellipse 7.png"},
+        {name:"Women’s Wear", class:"ellipse category2", path:"../assets/Ellipse 8.png"},
+        {name:"Winter Collections", class:"ellipse category3", path:"../assets/Ellipse 9.png"},
+        {name:"Hooded T-Shirts", class:"ellipse category4", path:"../assets/Ellipse 10.png"},
+        {name:"Polo Neck T-Shirts", class:"ellipse category5", path:"../assets/Ellipse 11.png"},
+        {name:"Full Sleeves T-Shirts", class:"ellipse category6", path:"../assets/Ellipse 12.png"},
     ]);
     return{
         categoryLoop
@@ -35,13 +40,31 @@ export default {
     font-style: normal;
     font-weight: 600;
 }
-.ellipse1{
+.ellipse{
   width: 170px;
   height: 170px;
   margin-right: 20px;
   border-radius: 170px;
-  background: #F1DBE9  url("../assets/Ellipse 7.png"); 
-  /*  background: #F1DBE9; */
 }
+.category1{
+  background: #F1DBE9  url("../assets/Ellipse 7.png"); 
+}
+.category2{
+  background: #F1DBE9  url("../assets/Ellipse 8.png"); 
+}
+.category3{
+  background: #F1DBE9  url("../assets/Ellipse 9.png"); 
+}
+.category4{
+  background: #F1DBE9  url("../assets/Ellipse 10.png"); 
+}
+.category5{
+  background: #F1DBE9  url("../assets/Ellipse 11.png"); 
+}
+.category6{
+  background: #F1DBE9  url("../assets/Ellipse 12.png"); 
+}
+
+
 
 </style>
