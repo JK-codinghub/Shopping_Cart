b6aa63f32f5021b1f0adf73382340caa6daa0c75
JK-codinghub <jyotickalashetty@gmail.com>
2023-11-18 19:51:43 +0530

    New categories and horizontal scroll implemented

--- src/components/ProductCategory.vue
+++ src/components/ProductCategory.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="row gx-0">
+  <div class="categoryRow gx-0 scrolling-wrapper row flex-row flex-nowrap ">
     <div class="col-2" v-for="(category,index) in categoryLoop" :key="index">
-        <div class="row">
+        <div class="row gx-0">
             <div class="col-12" :class="category.class">
              <!-- <img :src="categoryLoop[0].path" width="170" height="170" alt="">     -->
             </div>       
@@ -23,6 +23,12 @@ export default {
         {name:"Hooded T-Shirts", class:"ellipse category4", path:"../assets/Ellipse 10.png"},
         {name:"Polo Neck T-Shirts", class:"ellipse category5", path:"../assets/Ellipse 11.png"},
         {name:"Full Sleeves T-Shirts", class:"ellipse category6", path:"../assets/Ellipse 12.png"},
+        {name:"Men’s T-Shirtsgory1", class:"ellipse category1", path:"../assets/Ellipse 7.png"},
+        {name:"Women’s Wear", class:"ellipse category2", path:"../assets/Ellipse 8.png"},
+        {name:"Winter Collections", class:"ellipse category3", path:"../assets/Ellipse 9.png"},
+        {name:"Hooded T-Shirts", class:"ellipse category4", path:"../assets/Ellipse 10.png"},
+        {name:"Polo Neck T-Shirts", class:"ellipse category5", path:"../assets/Ellipse 11.png"},
+        {name:"Full Sleeves T-Shirts", class:"ellipse category6", path:"../assets/Ellipse 12.png"}
     ]);
     return{
         categoryLoop
@@ -32,6 +38,13 @@ export default {
 </script>
 
 <style scoped>
+
+.scrolling-wrapper{
+	overflow-x: auto;
+}
+::-webkit-scrollbar{
+    display: none;
+}
 .categoryStyle{
     margin-top: 20px;
     color: var(--Primary-Color_1, #1C1C1C);
